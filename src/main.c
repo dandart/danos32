0984db09640645de33ccc8a9a8c469e9439a8ed2
Dan <git@dandart.co.uk>
2014-01-20 20:37:30 +0000

    IDT GDT ISR

--- src/main.c
+++ src/main.c
@@ -7,5 +7,8 @@ int main(void) {
     clear();
     print("Welcome to DanOS32!");
     print(" Thanks for choosing us!");
+    asm volatile ("int $0x3");
+	asm volatile ("int $0x4");
+
     return 0;
 }
