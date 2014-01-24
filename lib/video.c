49fa9d3dae6ed6177c7a22545f802ebf23da1681
Dan <git@dandart.co.uk>
2014-01-24 08:41:05 +0000

    Add i8042

--- lib/video.c
+++ lib/video.c
@@ -9,11 +9,40 @@ void clear() {
     }
 }
 
+void printc(char ch) {
+    videoram[vrpos*2] = ch;
+    vrpos++;
+}
+
 void print(char* string) {
     short i = 0;
     do {
-        videoram[vrpos*2] = string[i];
-        vrpos++;
+        printc(string[i]);
         i++;
     } while ('\0' != string[i]);
+}
+
+void printch(char* string, short len) {
+    while (0 < len) {
+	printc(string[len]);
+        len--;
+    }
+}
+
+void printhex_le(char ch) {
+    char low;
+    char high;
+
+    low = 0x30 + (ch & 0x0f);
+    high = 0x30 + ((ch & 0xf0) >> 4);
+    
+   if (0x39 < low)
+        low += 0x07;
+
+    if (0x39 < high)
+        high += 0x07;
+
+    printc(high);
+    printc(low);
+    printc(0x20);
 }
--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include "video.h"
 #include "io.h"
-#include "clever.h"
+/* #include "clever.h" */
 #include "string.h"
+#include "8042.h"
 
 int main(void) {
     clear();
     print("Welcome to DanOS32!");
     print(" Thanks for choosing us!");
+    char ch = 0;
+    while(1) {
+	ch = getch_poll();
+	printch(&ch, 1);
+    }
     return 0;
 }
